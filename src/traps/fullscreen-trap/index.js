7cf138d973a2b07d6f28f756c8aca266d18d39ab
Trikolon <git@pbz.im>
2019-04-30 15:43:54 +0200

    Added another fullscreen trap

--- src/traps/fullscreen-trap/index.js
+++ src/traps/fullscreen-trap/index.js
@@ -2,5 +2,5 @@ import EvilTrap from '../../EvilTrap';
 
 const path = require('path');
 
-export default new EvilTrap('Fullscreen Trap', EvilTrap.CATEGORY.MISC, 'Trap the user in fullscreen by entering it whenever possible.')
+export default new EvilTrap('Fullscreen Traps', EvilTrap.CATEGORY.MISC, 'Several examples of trapping the user in fullscreen')
   .addStaticRoute('/', path.join(__dirname, 'static'));
