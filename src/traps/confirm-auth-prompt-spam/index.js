1b1703d131b7cec18a3c38d1407c78c7453b52ee
Trikolon <git@pbz.im>
2019-08-02 19:44:22 +0200

    Cleaned up Confirm Auth Prompt trap and added firefox bug number

--- src/traps/confirm-auth-prompt-spam/index.js
+++ src/traps/confirm-auth-prompt-spam/index.js
@@ -2,5 +2,6 @@ import EvilTrap from '../../EvilTrap';
 
 const path = require('path');
 
-export default new EvilTrap('Confirm Auth Prompt Spam', EvilTrap.CATEGORY.SPAM, 'Spam with auth confirmations prompts.', null)
+export default new EvilTrap('Confirm Auth Prompt Spam', EvilTrap.CATEGORY.SPAM,
+  'Spam with auth confirmations prompts.', { firefox: '1571003' })
   .addStaticRoute('/', path.join(__dirname, 'static'));
--- src/traps/confirm-auth-prompt-spam/static/index.js
+++ src/traps/confirm-auth-prompt-spam/static/index.js
@@ -1,8 +1,8 @@
-(async () => {
-  function navAuth() {
-    window.location.href = window.location.href
-      .replace(`${window.location.protocol}//`,
-        `${window.location.protocol}//admin@`);
-  }
-  setInterval(navAuth, 1000);
+(() => {
+  const nextLocation = window.location.href
+    .replace(`${window.location.protocol}//`,
+      `${window.location.protocol}//admin@`);
+  setInterval(() => {
+    window.location.href = nextLocation;
+  }, 1000);
 })();
