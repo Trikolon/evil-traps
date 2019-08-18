936a7d6cf23f7bd986cb2fd83a2ac8c5fda05c88
Trikolon <git@pbz.im>
2019-08-18 20:23:43 +0200

    Updated hello-world example to include addPageScript.

--- src/traps/hello-world/index.js
+++ src/traps/hello-world/index.js
@@ -10,9 +10,20 @@ function postHandler(req, res) {
   res.status(200).send('This is the hello world POST handler');
 }
 
+function helloWorldScript() {
+  // eslint-disable-next-line no-alert
+  alert('hello world!');
+}
+
 export default new EvilTrap('Hello World', EvilTrap.CATEGORY.MISC, 'A demo Evil Trap with static and dynamic content', undefined, true)
+  // Dynamic routes
   .routeBuilder((router) => {
     router.get('/', getHandler);
     router.post('/', postHandler);
   })
-  .addStaticRoute('/static', path.join(__dirname, 'static'));
+  // Static file route
+  .addStaticRoute('/static', path.join(__dirname, 'static'))
+  // Static script route passed a Function
+  .addScriptPage(helloWorldScript, '/in-page-script-func')
+  // Static script route passed a function string
+  .addScriptPage('alert("hello world str")', '/in-page-script-func-string');
