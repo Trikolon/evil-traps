0b08072256592bf45e3b782f201756167e8507e4
Trikolon <git@pbz.im>
2020-02-25 15:28:21 +0100

    insecure form: refactored for addScriptPage

--- src/traps/insecure-form-prompt/index.js
+++ src/traps/insecure-form-prompt/index.js
@@ -1,7 +1,20 @@
 import EvilTrap from '../../EvilTrap';
 
-const path = require('path');
-
 export default new EvilTrap('Insecure Form Submission Prompt Spam', EvilTrap.CATEGORY.SPAM,
   'Spam with insecure form submission prompts.', { firefox: '616849' })
-  .addStaticRoute('/', path.join(__dirname, 'static'));
+  .addScriptPage(() => {
+    // Make sure the page is loaded via https
+    if (window.location.protocol === 'http:') {
+      window.location.protocol = 'https';
+      return;
+    }
+
+    // Create a form which submits to a http page
+    const form = document.createElement('form');
+    form.action = `http://${window.location.host}${window.location.pathname}redirect`;
+    document.body.appendChild(form);
+    setInterval(() => { form.submit(); }, 10);
+  })
+  .addScriptPage(() => {
+    window.location.replace(window.location.href.replace('redirect', ''));
+  }, '/redirect');
